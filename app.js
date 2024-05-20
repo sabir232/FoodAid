f674a45b38a0acf59e437fc8c0d466fa0dc8409c
sabir <sabir@sabirs-MacBook-Air.local>
2024-05-20 19:15:50 +0530

    FoodAid First Commit

--- app.js
+++ app.js
@@ -14,31 +14,29 @@ require("dotenv").config();
 require("./config/dbConnection.js")();
 require("./config/passport.js")(passport);
 
-
-
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use("/assets", express.static(__dirname + "/assets"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({
-	secret: "secret",
-	resave: true,
-	saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(methodOverride("_method"));
 app.use((req, res, next) => {
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	res.locals.warning = req.flash("warning");
-	next();
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  res.locals.warning = req.flash("warning");
+  next();
 });
-
-
 
 // Routes
 app.use(homeRoutes);
@@ -46,10 +44,9 @@ app.use(authRoutes);
 app.use(donorRoutes);
 app.use(adminRoutes);
 app.use(agentRoutes);
-app.use((req,res) => {
-	res.status(404).render("404page", { title: "Page not found" });
+app.use((req, res) => {
+  res.status(404).render("404page", { title: "Page not found" });
 });
 
-
-const port = process.env.PORT || 5000;
+const port = 3001;
 app.listen(port, console.log(`Server is running at http://localhost:${port}`));
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDB = async() => {
-	try
-	{
-		const db = process.env.MONGO_URI;
-		await mongoose.connect(db);
-		console.log("MongoDB connected...");
-	}
-	catch(err)
-	{
-		console.log(err);
-		process.exit(1);
-	}
-}
+const connectDB = async () => {
+  try {
+    const db = "mongodb://localhost:27017/donations";
+    await mongoose.connect(db);
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
